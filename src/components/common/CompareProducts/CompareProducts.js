28beced2f4a57ea084cf42c7c22b920a7cccfbc8
Artur od Frontu <trapczynski.a@gmail.com>
2021-03-18 19:22:00 +0100

    Start to create CompareProducts component

---
+++ src/components/common/CompareProducts/CompareProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './CompareProducts.module.scss';
+
+const CompareProducts = () => {
+  return <div className={styles.component}></div>;
+};
+
+CompareProducts.propTypes = {};
+
+export default CompareProducts;
