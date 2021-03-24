ef39fa13d77a9267be54c61185d21452747eef5c
unknown <asiazmudziak@gmail.com>
2021-03-24 20:40:02 +0100

    add the Promotions section component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
+import Promotion from '../../features/Promotion/Promotion';
+
 import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
@@ -9,6 +11,7 @@ import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
+    <Promotion />
     <NewFurniture />
   </div>
 );
