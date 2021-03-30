670ad90426ba46a3b79d281b4bf77c4c77707f82
Artur od Frontu <trapczynski.a@gmail.com>
2021-03-30 23:56:39 +0200

    Finish Gallery component implementation (no functional)

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -16,39 +16,22 @@ import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons'
 
 class Gallery extends React.Component {
   state = {
-    activeTab: 'featured',
-    activePage: 0,
     activeProduct: {
-      id: 'aenean-ru-bristique-1',
-      name: 'Aenean Ru Bristique 1',
+      id: 'aenean-ru-bristique-6',
+      name: 'Aenean Ru Bristique 6',
       category: 'bed',
       price: 30,
       oldPrice: 45,
-      stars: 4,
+      stars: 2,
       promo: 'sale',
       newFurniture: true,
-      favorites: true,
-      compare: true,
+      favorites: false,
       image:
-        'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+        'https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80',
     },
+    activeTab: 'featured',
+    activePage: 0,
   };
-
-  setActiveProduct(item) {
-    this.setState({
-      activeProduct: {
-        id: item.id,
-        favorite: item.favorite,
-        image: item.image,
-        name: item.name,
-        newFurniture: item.newFurniture,
-        oldprice: item.oldprice,
-        price: item.price,
-        promo: item.promo,
-        stars: item.stars,
-      },
-    });
-  }
 
   render() {
     const { products } = this.props;
@@ -61,6 +44,7 @@ class Gallery extends React.Component {
     ];
 
     const { activeTab, activePage, activeProduct } = this.state;
+
     const slidesArr = products.filter(item => item.newFurniture === true);
     const slidesToDisplay = slidesArr.slice(activePage * 6, (activePage + 1) * 6);
 
@@ -68,7 +52,7 @@ class Gallery extends React.Component {
       <div className={style.root}>
         <div className='container'>
           <div className='row'>
-            <div className='col-lg-6 col-sm-12'>
+            <div className='col-6'>
               <div className={style.header}>
                 <h3>Furniture gallery</h3>
               </div>
@@ -87,30 +71,30 @@ class Gallery extends React.Component {
                 <div className={style.product}>
                   <img src={activeProduct.image} alt='product' />
                   <div className={style.buttons}>
-                    <ReactTooltip type='light' place='right' />
-                    <Button variant='gallery' data-tip='Add to favorites'>
+                    <ReactTooltip type='dark' place='right' />
+                    <Button data-tip='Add to favorites'>
                       <Icon icon={faHeart} className={style.icon}>
                         Favorite
                       </Icon>
                     </Button>
-                    <Button variant='gallery' data-tip='Add to compare'>
+                    <Button data-tip='Add to compare'>
                       <Icon icon={faExchangeAlt} className={style.icon}>
                         Add to compare
                       </Icon>
                     </Button>
-                    <Button variant='gallery' data-tip='View details'>
+                    <Button data-tip='View details'>
                       <Icon icon={faEye} className={style.icon}>
                         View details
                       </Icon>
                     </Button>
-                    <Button variant='gallery' data-tip='Add to basket'>
+                    <Button data-tip='Add to basket'>
                       <Icon icon={faShoppingBasket} className={style.icon}>
                         Add to basket
                       </Icon>
                     </Button>
                   </div>
                   <div className={style.content}>
-                    <div className={style.triangleTopLeft} />
+                    <div className={style.decorTopLeft} />
                     <h5>{activeProduct.name}</h5>
                     <div className={style.stars}>
                       {[1, 2, 3, 4, 5].map(stars => (
@@ -123,7 +107,7 @@ class Gallery extends React.Component {
                         </a>
                       ))}
                     </div>
-                    <div className={style.triangleBottomRight} />
+                    <div className={style.decorBottomRight} />
                     <div className={style.price}>
                       <h3>${activeProduct.price}</h3>
                       <h5>${activeProduct.oldPrice}</h5>
@@ -142,8 +126,8 @@ class Gallery extends React.Component {
                         alt='pic'
                         className={
                           slide.image === activeProduct.image
-                            ? style.slidePic + ' ' + style.active
-                            : style.slidePic
+                            ? style.slidePicture + ' ' + style.active
+                            : style.slidePicture
                         }
                       />
                     ))}
@@ -154,14 +138,14 @@ class Gallery extends React.Component {
                 </div>
               </div>
             </div>
-            <div className={'col-6 ' + style.staticPicture}>
-              <img src={products[5].image} alt='prod3' />
-              <div className={style.staticPictureDetails}>
+            <div className={'col-6 ' + style.picture}>
+              <img src={products[6].image} alt='prod3' />
+              <div className={style.pictureDetails}>
                 <h3>
-                  from <span>${products[5].price}</span>
+                  from <span>${products[6].price}</span>
                 </h3>
-                <h1>{products[5].name}</h1>
-                <Button variant='green'>Shop now</Button>
+                <h1>{products[6].name}</h1>
+                <Button className={style.shopNowBtn}>Shop now</Button>
               </div>
             </div>
           </div>
@@ -180,7 +164,7 @@ Gallery.propTypes = {
       image: PropTypes.string,
       name: PropTypes.string,
       newFurniture: PropTypes.bool,
-      oldprice: PropTypes.number,
+      oldPrice: PropTypes.number,
       price: PropTypes.number,
       promo: PropTypes.string,
       stars: PropTypes.number,
